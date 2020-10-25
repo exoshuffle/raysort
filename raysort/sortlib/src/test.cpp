17becc5c26c1f7796ffdc26b14cdd9f9ab855d26
Frank Luan <lsf@berkeley.edu>
2020-10-24 19:36:05 -0700

    Revive sortlib C++ test

--- raysort/sortlib/src/test.cpp
+++ raysort/sortlib/src/test.cpp
@@ -7,19 +7,30 @@
 
 using namespace sortlib;
 
-void print_record(const Record& rec) {
+void PrintRecord(const Record& rec) {
     for (size_t i = 0; i < HEADER_SIZE; ++i) {
         printf("%02x ", rec.header[i]);
     }
     printf("\n");
 }
 
-void assert_sorted(const Array<Record>& array) {
+void AssertSorted(const Array<Record>& array) {
     for (size_t i = 0; i < array.size - 1; ++i) {
         const auto& a = array.ptr[i];
         const auto& b = array.ptr[i + 1];
         assert(std::memcmp(a.header, b.header, HEADER_SIZE) <= 0);
     }
+}
+
+std::vector<Array<Record>> MakeRecordArrays(
+    Record* const records,
+    const std::vector<Partition>& parts) {
+    std::vector<Array<Record>> ret;
+    ret.reserve(parts.size());
+    for (const auto& part : parts) {
+        ret.emplace_back(Array<Record>{records + part.offset, part.size});
+    }
+    return ret;
 }
 
 void test() {
@@ -37,19 +48,14 @@ void test() {
     // Call and verify SortAndPartition().
     const auto& parts = SortAndPartition({records, num_records}, boundaries);
 
-    assert_sorted({records, num_records});
+    AssertSorted({records, num_records});
     assert(parts == std::vector<Partition>({{{0, 1}, {1, 0}, {1, 4}, {5, 3}}}));
 
-    std::vector<Array<Record>> record_arrays;
-    record_arrays.reserve(parts.size());
-    for (const auto& part : parts) {
-        record_arrays.emplace_back(
-            Array<Record>{records + part.offset, part.size});
-    }
+    const auto& record_arrays = MakeRecordArrays(records, parts);
 
     // Call and verify MergePartitions().
     const auto& merged_array = MergePartitions(record_arrays);
-    assert_sorted(merged_array);
+    AssertSorted(merged_array);
 }
 
 int main() {
@@ -59,36 +65,37 @@ int main() {
 
     printf("A-OK!\n");
 
-    // const size_t num_reducers = 1;
-    // const auto& boundaries = GetBoundaries(num_reducers);
+    const size_t num_reducers = 1;
+    const auto& boundaries = GetBoundaries(num_reducers);
 
-    // const size_t num_records = 10000;
-    // const size_t buf_size = RECORD_SIZE * (num_records + 1);
-    // void* buffer = malloc(buf_size);
+    const size_t num_records = 10000;
+    const size_t buf_size = RECORD_SIZE * (num_records + 1);
+    Record* records = new Record[num_records + 1];
 
-    // FILE* fin;
-    // size_t file_size = 0;
-    // fin = fopen("/var/tmp/raysort/input/input-0", "r");
-    // if (fin == NULL) {
-    //     perror("Failed to open file");
-    // } else {
-    //     file_size = fread(buffer, RECORD_SIZE, num_records, fin);
-    //     printf("Read %lu bytes.\n", file_size);
-    //     fclose(fin);
-    // }
+    FILE* fin;
+    size_t file_size = 0;
+    fin = fopen("/var/tmp/raysort/input/input-000000", "r");
+    if (fin == NULL) {
+        perror("Failed to open file");
+    } else {
+        file_size = fread(records, RECORD_SIZE, num_records, fin);
+        printf("Read %lu bytes.\n", file_size);
+        fclose(fin);
+    }
 
-    // const auto& record_arrays =
-    //     SortAndPartition({(Record*)buffer, num_records}, boundaries);
-    // const auto output = MergePartitions(record_arrays);
+    const auto& parts = SortAndPartition({records, num_records}, boundaries);
+    const auto& record_arrays = MakeRecordArrays(records, parts);
+    const auto output = MergePartitions(record_arrays);
 
-    // FILE* fout;
-    // fout = fopen("/var/tmp/raysort/output/test-output", "w");
-    // if (fout == NULL) {
-    //     perror("Failed to open file");
-    // } else {
-    //     size_t writecount = fwrite(output.ptr, RECORD_SIZE, output.size,
-    //     fout); printf("Wrote %lu bytes.\n", writecount); fclose(fout);
-    // }
+    FILE* fout;
+    fout = fopen("/var/tmp/raysort/output/test-output", "w");
+    if (fout == NULL) {
+        perror("Failed to open file");
+    } else {
+        size_t writecount = fwrite(output.ptr, RECORD_SIZE, output.size, fout);
+        printf("Wrote %lu bytes.\n", writecount);
+        fclose(fout);
+    }
 
     return 0;
 }
