3d5283f097a617825ba96a5aa9f977af08471dc8
Frank Luan <lsf@berkeley.edu>
2022-01-11 20:11:41 +0000

    Differentiate between raysort and spark clusters

--- terraform/aws/aws.tf
+++ terraform/aws/aws.tf
@@ -15,9 +15,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "raysort_worker" {
-  count = 4
+  count = 8
 
-  ami           = "ami-02ccbf3107d9174a7"
+  ami           = "ami-02ccbf3107d9174a7" # ubuntu2004-gcc-conda-py39-raysort
   instance_type = "m6i.4xlarge"
   key_name      = "login-us-west-2"
 
@@ -30,6 +30,27 @@ resource "aws_instance" "raysort_worker" {
   }
 
   tags = {
-    Name = "raysort-worker-${count.index}"
+    Name = "raysort-worker-${format("%03d", count.index)}"
+  }
+}
+
+
+resource "aws_instance" "spark_worker" {
+  count = 8
+
+  ami           = "ami-0ed1b8333dc630af0" # raysort-spark-yarn
+  instance_type = "m6i.4xlarge"
+  key_name      = "login-us-west-2"
+
+  ebs_block_device {
+    device_name = "sdb"
+    iops        = 3000
+    throughput  = 500
+    volume_size = 1000
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "spark-worker-${format("%03d", count.index)}"
   }
 }
