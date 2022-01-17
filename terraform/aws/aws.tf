16d84f81a07053aaef8c768cd1455c70545e22d9
Frank Luan <lsf@berkeley.edu>
2022-01-17 19:31:40 +0000

    Remove Spark workers from aws.tf

--- terraform/aws/aws.tf
+++ terraform/aws/aws.tf
@@ -33,24 +33,3 @@ resource "aws_instance" "raysort_worker" {
     Name = "raysort-worker-${format("%03d", count.index)}"
   }
 }
-
-
-resource "aws_instance" "spark_worker" {
-  count = 8
-
-  ami           = "ami-0ed1b8333dc630af0" # raysort-spark-yarn
-  instance_type = "m6i.4xlarge"
-  key_name      = "login-us-west-2"
-
-  ebs_block_device {
-    device_name = "sdb"
-    iops        = 3000
-    throughput  = 500
-    volume_size = 1000
-    volume_type = "gp3"
-  }
-
-  tags = {
-    Name = "spark-worker-${format("%03d", count.index)}"
-  }
-}
