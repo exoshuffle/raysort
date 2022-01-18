97cabb3faf2d822088c6868290e93d5942373ce4
Frank Luan <lsf@berkeley.edu>
2022-01-18 02:14:57 +0000

    Pytest setup

---
+++ raysort/tests/test_tracing_utils.py
@@ -0,0 +1,13 @@
+import ray
+
+from raysort import tracing_utils
+
+
+def test_get_spilling_stats():
+    try:
+        ray.init()
+        ret = tracing_utils._get_spilling_stats()
+        assert ret["spilled_gb"] == 0, ret
+        assert ret["restored_gb"] == 0, ret
+    finally:
+        ray.shutdown()
