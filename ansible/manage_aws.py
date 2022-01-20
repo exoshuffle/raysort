00baa734b85f7d85b4c6fc73b893b7d4781c6703
Frank Luan <lsf@berkeley.edu>
2022-01-20 18:52:49 +0000

    Add script for managing AWS instances (start/stop)

---
+++ ansible/manage_aws.py
@@ -0,0 +1,55 @@
+from typing import Dict, List
+
+from absl import app
+from absl import flags
+from absl import logging
+import boto3
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    "instance_name_pattern",
+    "raysort-worker-*",
+    "instance name pattern",
+)
+flags.DEFINE_enum(
+    "action",
+    "stop",
+    ["start", "stop"],
+    "action to perform on the instances",
+)
+
+
+def get_instances(pattern: str) -> List[Dict]:
+    ec2 = boto3.client("ec2")
+    resp = ec2.describe_instances(
+        Filters=[
+            {
+                "Name": "tag:Name",
+                "Values": [pattern],
+            },
+        ],
+        MaxResults=1000,
+    )
+    return [item["Instances"][0] for item in resp["Reservations"]]
+
+
+def perform_action(action: str, instances: List[Dict]):
+    ids = [inst["InstanceId"] for inst in instances]
+    ec2 = boto3.client("ec2")
+    action_to_fn = {
+        "start": ec2.start_instances,
+        "stop": ec2.stop_instances,
+    }
+    fn = action_to_fn[action]
+    resp = fn(InstanceIds=ids)
+    logging.info(resp)
+
+
+def main(argv):
+    del argv  # Unused.
+    instances = get_instances(FLAGS.instance_name_pattern)
+    perform_action(FLAGS.action, instances)
+
+
+if __name__ == "__main__":
+    app.run(main)
