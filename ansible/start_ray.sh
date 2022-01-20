b46032ba7fc0bc6da13b43f5e2285fb159d6f9b0
Frank Luan <lsf@berkeley.edu>
2022-01-20 00:01:59 +0000

    Remove --object_store_memory from head node spec

--- ansible/start_ray.sh
+++ ansible/start_ray.sh
@@ -16,8 +16,7 @@ ray start --head --port=6379 \
     --object-manager-port=8076 \
     --metrics-export-port=8090 \
     --resources='{"head":1}' \
-    --system-config='{"max_io_workers":8,"object_spilling_threshold":1,"object_spilling_config":"{\"type\":\"filesystem\",\"params\":{\"directory_path\":[\"/mnt/nvme0/tmp/ray\"]}}"}' \
-    --object-store-memory=30064771072
+    --system-config='{"max_io_workers":8,"object_spilling_threshold":1,"object_spilling_config":"{\"type\":\"filesystem\",\"params\":{\"directory_path\":[\"/mnt/nvme0/tmp/ray\"]}}"}'
 
 HEAD_IP=$(ec2metadata --local-ipv4)
 ansible-playbook "$SCRIPT_DIR/ray.yml" -i "$SCRIPT_DIR/_$CLOUD.yml" --extra-vars "{\"head_ip\":\"$HEAD_IP\"}"
