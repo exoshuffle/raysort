5f34b054f8170c4b9f9b7a83117c79e2e9b6100a
Frank Luan <lsf@berkeley.edu>
2022-01-21 22:28:08 +0000

    Speed up Ansible

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CLOUD=aws
 
 python $SCRIPT_DIR/update_inventory.py
-ansible-playbook $SCRIPT_DIR/setup_$CLOUD.yml -i $SCRIPT_DIR/_$CLOUD.yml
+ansible-playbook -f $(nproc) $SCRIPT_DIR/setup_$CLOUD.yml -i $SCRIPT_DIR/_$CLOUD.yml
--- ansible/start_spark.sh
+++ ansible/start_spark.sh
@@ -5,7 +5,7 @@ set -ex
 CLOUD=aws_spark
 
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
- 
+
 $SPARK_HOME/sbin/stop-history-server.sh
 
 $HADOOP_HOME/sbin/stop-yarn.sh
@@ -15,7 +15,7 @@ $HADOOP_HOME/bin/hdfs namenode -format -force
 python "$DIR/update_inventory.py"
 # After formatting namenode, the datanodes are left with a different file system.
 # The spark.yml will remove existing data directory which contains incorrect metadata.
-ansible-playbook -i "$DIR/_$CLOUD.yml" "$DIR/spark.yml"
+ansible-playbook -f $(nproc) -i "$DIR/_$CLOUD.yml" "$DIR/spark.yml"
 
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
