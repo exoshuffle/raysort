ce15fadec9e982503a2055cc07730d1bfcab6f1b
Sean Kim <seankimkdy@berkeley.edu>
2022-01-22 01:02:50 -0800

    Revert default to aws

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -4,7 +4,7 @@ set -ex
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-CLOUD=aws_spark
+CLOUD=aws
 
 python $SCRIPT_DIR/update_inventory.py --cloud=$CLOUD
 python $SCRIPT_DIR/update_workers.py
