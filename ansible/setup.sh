854500c300b8eed103388a70723cb2080e177689
Frank Luan <lsf@berkeley.edu>
2022-01-25 18:11:27 +0000

    Fix setup.sh for aws

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -7,5 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CLOUD=aws
 
 python $SCRIPT_DIR/update_inventory.py --cloud=$CLOUD
-python $SCRIPT_DIR/update_workers.py
+if [ $CLOUD == "aws_spark" ]; then
+    python $SCRIPT_DIR/update_workers.py
+fi
 ansible-playbook -f $(($(nproc) * 4)) $SCRIPT_DIR/setup_$CLOUD.yml -i $SCRIPT_DIR/_$CLOUD.yml
