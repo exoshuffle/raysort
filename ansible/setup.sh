ff05e3268065f03b043419863354ecf3c3be104a
Frank Luan <lsf@berkeley.edu>
2022-01-27 06:54:28 +0000

    Add PYTHONUNBUFFERED to envrc

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -7,7 +7,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CLOUD=aws
 
 python $SCRIPT_DIR/update_inventory.py --cloud=$CLOUD
-if [ $CLOUD == "aws_spark" ]; then
-    python $SCRIPT_DIR/update_workers.py
-fi
+# TODO: update_inventory.py should also make a prometheus discovery file
+# TODO: update_workers.py needs to take in --cloud
+python $SCRIPT_DIR/update_workers.py
+# TODO: fill in core-site.xml and yarn-site.xml automatically.
 ansible-playbook -f $(($(nproc) * 4)) $SCRIPT_DIR/setup_$CLOUD.yml -i $SCRIPT_DIR/_$CLOUD.yml
