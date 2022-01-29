a270953e22f4a719ac4f9e4c16a7d632a58db1a8
Frank Luan <lsf@berkeley.edu>
2022-01-28 23:49:13 -0800

    Make --use_object_store the default; Add Magnet (#17)
    * Use default max_io_workers
    
    * Rename and Magnet

--- ansible/start_ray.sh
+++ ansible/start_ray.sh
@@ -16,8 +16,8 @@ ray start --head --port=6379 \
     --object-manager-port=8076 \
     --metrics-export-port=8090 \
     --resources='{"head":1}' \
-    --system-config='{"max_io_workers":8,"object_spilling_threshold":1,"object_spilling_config":"{\"type\":\"filesystem\",\"params\":{\"directory_path\":[\"/mnt/nvme0/tmp/ray\"]}}"}'
-    # --system-config='{"send_unpin":true,"max_io_workers":8,"object_spilling_threshold":1,"object_spilling_config":"{\"type\":\"filesystem\",\"params\":{\"directory_path\":[\"/mnt/nvme0/tmp/ray\"]}}"}'
+    --system-config='{"object_spilling_threshold":1,"object_spilling_config":"{\"type\":\"filesystem\",\"params\":{\"directory_path\":[\"/mnt/nvme0/tmp/ray\"]}}"}'
+    # --system-config='{"send_unpin":true,"object_spilling_threshold":1,"object_spilling_config":"{\"type\":\"filesystem\",\"params\":{\"directory_path\":[\"/mnt/nvme0/tmp/ray\"]}}"}'
 
 HEAD_IP=$(ec2metadata --local-ipv4)
 ansible-playbook -f $(($(nproc) * 4)) "$SCRIPT_DIR/ray.yml" -i "$SCRIPT_DIR/_$CLOUD.yml" --extra-vars "{\"head_ip\":\"$HEAD_IP\"}"
