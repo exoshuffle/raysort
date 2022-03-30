cbbb6225da3cc919f8f904e712f66d72591c97b4
Frank Luan <lsf@berkeley.edu>
2022-03-30 22:39:49 +0000

    Fix Spark and YARN setup

--- scripts/config/ansible/start_spark.sh
+++ scripts/config/ansible/start_spark.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-CLOUD=aws
-
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 export HADOOP_SSH_OPTS="-i ~/.aws/login-us-west-2.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR"
@@ -14,10 +12,6 @@ $HADOOP_HOME/sbin/stop-yarn.sh
 $HADOOP_HOME/sbin/stop-dfs.sh
 
 $HADOOP_HOME/bin/hdfs namenode -format -force
-
-# After formatting namenode, the datanodes are left with a different file system.
-# The spark.yml will remove existing data directory which contains incorrect metadata.
-ansible-playbook -f $(($(nproc) * 4)) -i "$DIR/_raysort-lsf.yml" "$DIR/spark.yml"
 
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
