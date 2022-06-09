7c4a05b1bf927bc640b73346d9aa562661279c31
Isaac Ong <isaacong.jw@gmail.com>
2022-06-08 18:03:04 -0700

    Linting fixes

--- scripts/config/cluster/setup_monitoring.py
+++ scripts/config/cluster/setup_monitoring.py
@@ -1,12 +1,9 @@
 import logging
-import ray
 import socket
-from scripts.cls import (
-    setup_prometheus,
-    setup_grafana,
-    sleep,
-    get_prometheus_sd_content,
-)
+
+import ray
+
+from scripts import cls
 
 
 def get_ips():
@@ -24,15 +21,15 @@ def main():
     head_ip = socket.gethostbyname(socket.gethostname())
     ips = get_ips()
 
-    setup_prometheus(head_ip, ips)
-    setup_grafana()
+    cls.setup_prometheus(head_ip, ips)
+    cls.setup_grafana()
 
     while True:
-        sleep(60, "monitoring worker IPs")
+        cls.sleep(60, "monitoring worker IPs")
         ips = get_ips()
         logging.info("updated service discovery file with IPs ", ips)
         with open("/tmp/prometheus/service_discovery.json", "w") as fout:
-            fout.write(get_prometheus_sd_content(head_ip, ips))
+            fout.write(cls.get_prometheus_sd_content(head_ip, ips))
 
 
 if __name__ == "__main__":
