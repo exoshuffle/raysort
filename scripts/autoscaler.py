4a4046811c5af39177718627bac89def9989ea8b
Isaac Ong <isaacong.jw@gmail.com>
2022-07-12 01:55:34 -0700

    Add command for running once

--- scripts/autoscaler.py
+++ scripts/autoscaler.py
@@ -4,7 +4,7 @@ import string
 import sys
 
 import click
-from util import run
+import util
 
 SCRIPT_DIR = pathlib.Path(os.path.dirname(__file__))
 AUTOSCALER_DIR = SCRIPT_DIR / "config" / "autoscaler"
@@ -42,13 +42,40 @@ def get_or_create_autoscaler_config(cluster_name: str) -> pathlib.Path:
     return config_path
 
 
+def run_once(config_path: str):
+    util.run(f"ray up -y '{config_path}'")
+    check_ready(config_path)
+    util.run(f"ray submit '{config_path}' raysort/main.py")
+    util.run(f"ray down -y '{config_path}'")
+
+
+def check_ready(config_path, num_tries=7, wait_time=60):
+    while num_tries > 0:
+        status = util.run_output(
+            f"ray exec '{config_path}' 'conda activate raysort && ray status'"
+        )
+        if "no pending nodes" in status:
+            click.echo(f"Ray cluster is ready: {status}")
+            return
+        click.echo(
+            f"Ray cluster is not ready yet, sleeping for {wait_time} secs, current status=\n{status}"
+        )
+        util.sleep(wait_time, "worker nodes starting up...")
+        num_tries -= 1
+    util.run(f"ray down -y '{config_path}'")
+    raise RuntimeError("Ray cluster is not ready")
+
+
 def main():
     cluster_name = os.getenv("CLUSTER_NAME")
     assert cluster_name is not None, "CLUSTER_NAME must be set for autoscaler"
     assert len(sys.argv) > 1, "Must specify a command for ray autoscaler e.g. up"
     config_path = get_or_create_autoscaler_config(cluster_name)
 
-    run(f"ray {sys.argv[1]} '{config_path}' {' '.join(sys.argv[2:])}")
+    if sys.argv[1] == "run_once":
+        run_once(config_path)
+    else:
+        util.run(f"ray {sys.argv[1]} '{config_path}' {' '.join(sys.argv[2:])}")
 
 
 if __name__ == "__main__":
