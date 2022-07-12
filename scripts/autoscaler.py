bfc437dd0a58f487dd805a1c8753c7246678a6d7
Isaac Ong <isaacong.jw@gmail.com>
2022-07-12 02:00:36 -0700

    Create autoscaler config based on raysort config

--- scripts/autoscaler.py
+++ scripts/autoscaler.py
@@ -17,13 +17,13 @@ VARIABLES_TO_REPLACE = [
 ]
 
 
-def get_or_create_autoscaler_config(cluster_name: str) -> pathlib.Path:
-    config_path = AUTOSCALER_DIR / f"_{cluster_name}-autoscaler.yaml"
-    if os.path.exists(config_path):
-        click.echo(f"Found existing configuration for {cluster_name}")
-        return config_path
+def get_or_create_autoscaler_config(raysort_config: str) -> pathlib.Path:
+    autoscaler_config_path = AUTOSCALER_DIR / f"_{raysort_config}-autoscaler.yaml"
+    if os.path.exists(autoscaler_config_path):
+        click.echo(f"Found existing configuration for {raysort_config} config")
+        return autoscaler_config_path
 
-    assert not os.path.exists(config_path), f"{config_path} must not exist"
+    assert not os.path.exists(autoscaler_config_path), f"{autoscaler_config_path} must not exist"
     assert os.path.exists(
         AUTOSCALER_CONFIG_TEMPLATE_PATH
     ), f"{AUTOSCALER_CONFIG_TEMPLATE_PATH} must exist"
@@ -31,22 +31,22 @@ def get_or_create_autoscaler_config(cluster_name: str) -> pathlib.Path:
     with open(AUTOSCALER_CONFIG_TEMPLATE_PATH, "r") as template_file:
         template = string.Template(template_file.read())
 
-    config = template.substitute(
+    autoscaler_config = template.substitute(
         **{var: os.getenv(var) for var in VARIABLES_TO_REPLACE}
     )
 
-    with open(config_path, "w") as config_file:
-        config_file.write(config)
+    with open(autoscaler_config_path, "w") as config_file:
+        config_file.write(autoscaler_config)
 
-    click.echo(f"Created autoscaler config file for {cluster_name}")
-    return config_path
+    click.echo(f"Created autoscaler config file for {raysort_config} config")
+    return autoscaler_config_path
 
 
 def main():
-    cluster_name = os.getenv("CLUSTER_NAME")
-    assert cluster_name is not None, "CLUSTER_NAME must be set for autoscaler"
+    raysort_config = os.getenv("CONFIG")
+    assert raysort_config is not None, "CONFIG must be set for autoscaler"
     assert len(sys.argv) > 1, "Must specify a command for ray autoscaler e.g. up"
-    config_path = get_or_create_autoscaler_config(cluster_name)
+    config_path = get_or_create_autoscaler_config(raysort_config)
 
     run(f"ray {sys.argv[1]} '{config_path}' {' '.join(sys.argv[2:])}")
 
