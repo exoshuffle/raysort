dd320bf7b47838bfb0d78d3874b52115243d560b
Isaac Ong <isaacong.jw@gmail.com>
2022-10-25 14:04:12 -0700

    Add function to calculate boundaries

---
+++ raysort/tests/test_calculate_boundaries.py
@@ -0,0 +1,42 @@
+from raysort import sort_utils
+
+
+def test_calculate_boundaries_uniform():
+    # samples = [0, 64, 196, 255]
+    samples = [
+        b"\x00" * 100,
+        b"\x40" + b"\x00" * 99,
+        b"\xC4" + b"\x00" * 99,
+        b"\xFF" + b"\x00" * 99,
+    ]
+    assert sort_utils.calculate_boundaries(samples, 2, bytes_for_bounds=1) == [
+        0,
+        130,
+        255,
+    ]
+    # Ensure that bounds overlap with when number of partitions increases by a mu;tiple
+    assert sort_utils.calculate_boundaries(samples, 4, bytes_for_bounds=1) == [
+        0,
+        16,
+        130,
+        240.25,
+        255,
+    ]
+
+
+def test_calculate_boundaries_skewed():
+    # samples = [1, 2]
+    samples = [b"\x01" + b"\x00" * 99, b"\x02" + b"\x00" * 99]
+    assert sort_utils.calculate_boundaries(samples, 2, bytes_for_bounds=1) == [
+        0,
+        1.5,
+        255,
+    ]
+    # Ensure that bounds overlap with when number of partitions increases by a mu;tiple
+    assert sort_utils.calculate_boundaries(samples, 4, bytes_for_bounds=1) == [
+        0,
+        0.75,
+        1.5,
+        65.25,
+        255,
+    ]
