5d0aab268dbc3118c7533ee92fa555dd62da2e47
Frank Luan <lsf@berkeley.edu>
2022-10-29 05:14:49 +0000

    Add config for object_manager_max_bytes_in_flight

--- raysort/config/common.py
+++ raysort/config/common.py
@@ -72,6 +72,8 @@ class SystemConfig:
     _cluster: InitVar[ClusterConfig]
     max_fused_object_count: int = 2000
     object_spilling_threshold: float = 0.8
+    object_manager_max_bytes_in_flight_percent: float = 0.05
+    object_manager_max_bytes_in_flight: int = field(init=False)
     # How much system memory to allocate for the object store.
     object_store_memory_percent: float = 0.6
     object_store_memory_bytes: int = field(init=False)
@@ -82,6 +84,10 @@ class SystemConfig:
     s3_spill: int = 0
 
     def __post_init__(self, cluster: ClusterConfig):
+        self.object_manager_max_bytes_in_flight = int(
+            cluster.instance_type.memory_bytes
+            * self.object_manager_max_bytes_in_flight_percent
+        )
         self.object_store_memory_bytes = int(
             cluster.instance_type.memory_bytes * self.object_store_memory_percent
         )
