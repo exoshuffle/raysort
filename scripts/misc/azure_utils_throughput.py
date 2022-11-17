c31edb75aab701eb2959184054448ccbf1022765
tonyhong007 <tonyhong007@gmail.com>
2022-11-17 08:57:45 +0000

    azure throughput benchmark

--- scripts/misc/azure_utils_throughput.py
+++ scripts/misc/azure_utils_throughput.py
@@ -1,6 +1,8 @@
 import time
 
+import io
 import ray
+import numpy as np
 
 from raysort import azure_utils
 from typing import List
@@ -13,37 +15,72 @@ from raysort import (
 from raysort.config import AppConfig
 from raysort.typing import PartInfo
 
-@ray.remote
-def upload():
-    azure_utils.upload()
+PART_SIZE = 2_000_000_000
+DOWNLOAD_PART_SIZE = 1_997_200_000
+NUM_TASKS = 500
+CONCURRENCY: int = 10
+OFFSET = 64
+
 
 @ray.remote
-def download(pinfo: PartInfo):
-    azure_utils.download(pinfo)
+def upload(i):
+    new_i = i % OFFSET
+    blob_client = azure_utils.get_blob_client("raysort-lsf", f"{new_i:03}")
+    data = b"0" * PART_SIZE
+    blob_client.upload_blob(data, overwrite=True, max_concurrency=CONCURRENCY)
 
-def analyze_uploads(cfg: AppConfig, parts: List[PartInfo]):
-    # TODO: reference generate input?
-    return
 
-def analyze_downloads(cfg: AppConfig, parts: List[PartInfo]):
+@ray.remote
+def download(i):
+    new_i = i % OFFSET
+    blob_client = azure_utils.get_blob_client("raysort-lsf", f"{new_i:03}")
+    stream = blob_client.download_blob(max_concurrency=CONCURRENCY)
+    buf = io.BytesIO()
+    stream.readinto(buf)
+    return np.frombuffer(buf.getbuffer(), dtype=np.uint8)
+
+
+def analyze_uploads():
+    print("Analyzing Azure throughput on uploads")
     begin = time.time()
-    tasks = [download.remote(parts[part_id]) for part_id in range(cfg.num_mappers)]
+    tasks = [upload.remote(i) for i in range(NUM_TASKS)]
     not_ready = tasks
     num_completed = 0
     while not_ready:
         ready, not_ready = ray.wait(not_ready)
         num_completed += len(ready)
         duration = time.time() - begin
-        print(duration, "s", PART_SIZE * num_completed / duration / 1024 / 1024 * 8, "Mb/s")
+        print(
+            duration,
+            "s",
+            PART_SIZE * num_completed / duration / 1024 / 1024 * 8,
+            "Mb/s",
+        )
+    return
+
+
+def analyze_downloads():
+    print("Analyzing Azure throughput on downloads")
+    begin = time.time()
+    tasks = [download.remote(i) for i in range(NUM_TASKS)]
+    not_ready = tasks
+    num_completed = 0
+    while not_ready:
+        ready, not_ready = ray.wait(not_ready)
+        num_completed += len(ready)
+        duration = time.time() - begin
+        print(
+            duration,
+            "s",
+            DOWNLOAD_PART_SIZE * num_completed / duration / 1024 / 1024 * 8,
+            "Mb/s",
+        )
+    return
 
 
 def main():
-    job_cfg = config.get()
-    cfg = job_cfg.app
-    parts = sort_utils.load_manifest(cfg)
-    analyze_uploads(cfg, parts)
-    analyze_downloads(cfg, parts)
-
+    analyze_uploads()
+    analyze_downloads()
 
 
 if __name__ == "__main__":
