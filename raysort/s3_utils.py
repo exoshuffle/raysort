8e35616dd21d00c48e9bd9b0d84f0c164f432ec9
Frank Luan <lsf@berkeley.edu>
2022-12-06 23:22:53 +0000

    Remove deprecated code

--- raysort/s3_utils.py
+++ raysort/s3_utils.py
@@ -10,7 +10,7 @@ import botocore
 import numpy as np
 from boto3.s3 import transfer
 
-from raysort import constants, s3_custom, sort_utils
+from raysort import constants, s3_custom
 from raysort.config import AppConfig
 from raysort.typing import PartInfo, Path
 
@@ -105,71 +105,9 @@ def upload_s3_buffer(
     client().upload_fileobj(io.BytesIO(data), pinfo.bucket, pinfo.path, Config=config)
 
 
-def single_upload(
-    cfg: AppConfig, pinfo: PartInfo, merger: Iterable[np.ndarray]
-) -> list[PartInfo]:
-    buf = io.BytesIO()
-    for datachunk in merger:
-        buf.write(datachunk)
-    upload_s3_buffer(cfg, buf.getbuffer(), pinfo)
-    return [pinfo]
-
-
-def multi_upload(
-    cfg: AppConfig, pinfo: PartInfo, merger: Iterable[np.ndarray]
-) -> list[PartInfo]:
-    parallelism = cfg.reduce_io_parallelism
-    upload_threads = []
-    mpu_queue = queue.PriorityQueue()
-    chunk_id = 0
-
-    def _upload(data, chunk_id):
-        sub_part_id = constants.merge_part_ids(pinfo.part_id, chunk_id, skip_places=2)
-        sub_pinfo = sort_utils.part_info(cfg, sub_part_id, kind="output", cloud=True)
-        upload_s3_buffer(cfg, data, sub_pinfo, use_threads=False)
-        mpu_queue.put(sub_pinfo)
-
-    def _upload_part(data):
-        nonlocal chunk_id
-        if len(upload_threads) >= parallelism > 0:
-            upload_threads.pop(0).join()
-        if parallelism > 0:
-            thd = threading.Thread(target=_upload, args=(data, chunk_id))
-            thd.start()
-            upload_threads.append(thd)
-        else:
-            _upload(data, chunk_id)
-        chunk_id += 1
-
-    # The merger produces a bunch of small chunks towards the end, which
-    # we need to fuse into one chunk before uploading.
-    tail = io.BytesIO()
-    for datachunk in merger:
-        if datachunk.size >= constants.S3_MIN_CHUNK_SIZE:
-            # There should never be large chunks once we start seeing
-            # small chunks towards the end.
-            assert tail.getbuffer().nbytes == 0
-            _upload_part(datachunk.tobytes())
-        else:
-            tail.write(datachunk)
-
-    if tail.getbuffer().nbytes > 0:
-        tail.seek(0)
-        _upload_part(tail.getbuffer())
-
-    # Wait for all upload tasks to complete.
-    for thd in upload_threads:
-        thd.join()
-
-    return list(mpu_queue.queue)
-
-
 def multipart_upload(
     cfg: AppConfig, pinfo: PartInfo, merger: Iterable[np.ndarray]
 ) -> list[PartInfo]:
-    # TODO(@lsf) make this a flag
-    # return single_upload(cfg, pinfo, merger)
-    # return multi_upload(cfg, pinfo, merger)
     parallelism = cfg.reduce_io_parallelism
     s3_client = client()
     mpu = s3_client.create_multipart_upload(Bucket=pinfo.bucket, Key=pinfo.path)
