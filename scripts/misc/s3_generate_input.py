6ef9b61ae1647d8e3c3c606c367e7f7730105224
Derek Luan <derek.t.luan@gmail.com>
2022-12-24 21:36:35 +0000

    reformat script

--- scripts/misc/s3_generate_input.py
+++ scripts/misc/s3_generate_input.py
@@ -1,6 +1,6 @@
-'''
+"""
 Fills the s3 buckets with the input gensort input
-'''
+"""
 
 import logging
 
@@ -15,12 +15,14 @@ from raysort import (
 )
 from raysort.config import JobConfig
 
+
 def init(job_cfg: JobConfig) -> ray.actor.ActorHandle:
     logging_utils.init()
     logging.info("Job config: %s", job_cfg.name)
     ray_utils.init(job_cfg)
     sort_utils.init(job_cfg.app)
     return tracing_utils.create_progress_tracker(job_cfg)
+
 
 def main():
     job_cfg = config.get()
