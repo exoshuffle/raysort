376f930d72b0ed07101d190818be4e12c00ed873
Derek Luan <derek.t.luan@gmail.com>
2023-01-11 23:04:00 +0000

    comment out subnet

--- scripts/config/terraform/aws-template/main.tf
+++ scripts/config/terraform/aws-template/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "cluster" {
   associate_public_ip_address = true
   # subnet_id                   = "subnet-084e54bf496121333" # exoshuffle-subnet-public1-us-west-2a
   # subnet_id                   = "subnet-0af4497f8b2bffeaa" # exoshuffle-subnet-public1-us-west-2b
-  subnet_id = "subnet-033a4bd05bcc8c21e" # exoshuffle-subnet-public1-us-west-2c
+  # subnet_id = "subnet-033a4bd05bcc8c21e" # exoshuffle-subnet-public1-us-west-2c
   # subnet_id                   = "subnet-05b20e1a041f5fd44" # exoshuffle-subnet-public2-us-west-2d
 
   root_block_device {
