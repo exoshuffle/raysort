d0780847256b3f5771eda921e2a842b14e4f5775
Derek Luan <derek.t.luan@gmail.com>
2023-08-12 23:55:40 +0000

    reformat page_views

--- raysort/archived/page_views.py
+++ raysort/archived/page_views.py
@@ -51,7 +51,7 @@ def load_partition(cfg: AppConfig, part_id: int) -> pd.DataFrame:
         sep=" ",
         names=["language", "title", "requests", "size"],
         quoting=csv.QUOTE_NONE,
-        iterator=False
+        iterator=False,
     )
     return df
 
